c60f311fe1f208caccc5c6adfb80fa8066367213
Evans Rutto <robbyevans001@gmail.com>
2023-02-05 22:27:49 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Alert from './components/Alert'
+import Nav from './components/Nav'
+
+
+function App() {
+  return (
+    <div>
+  <Alert/>
+  <Nav/>
+    </div>
+    
+//     <Alert
+//   color="failure"
+//   icon={HiInformationCircle}
+// >
+//   <span>
+//     <span className="font-medium">
+//       Info alert!
+//     </span>
+//     {' '}Change a few things up and try submitting again.
+//   </span>
+// </Alert>
+
+  
+     
+
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+        md: '768px',
+        lg: '976px',
+        xl: '1440px',
+      },
+      colors:{
+        primary:"#ff4800",
+        background:"#f1f3f4"
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
